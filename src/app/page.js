c41696881040c20ce8504da7cc6a2ca07520cd2e
souldev003 <mehrabhosen003@gmail.com>
2026-05-18 16:38:58 +0600

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@heroui/react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
+}
